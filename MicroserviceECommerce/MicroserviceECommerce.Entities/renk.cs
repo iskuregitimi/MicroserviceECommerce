784d7420d7be49369e250ed7a6eefde22ca93274
Administrator <Administrator@DESKTOP-O269UOM>
2019-06-14 14:23:21 +0300

    MicroserviceECommerce.ECommerce.WebApi  dosyasında SwaggerConfig  yapıldı ancak app data da cosya gelmedi

---
+++ MicroserviceECommerce/MicroserviceECommerce.Entities/renk.cs
@@ -0,0 +1,17 @@
+namespace MicroserviceECommerce.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("renk")]
+    public partial class renk
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string RenkAdi { get; set; }
+    }
+}
