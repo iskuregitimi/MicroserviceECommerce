9dd022fb1e0bb85c3c61d424a0d7ffb6ea22d486
fatih kaya <fatihkayabilgeadam@gmail.com>
2019-06-17 23:36:46 +0300

    Customer List İşlemi yapıldı

--- MicroserviceECommerce/MicroserviceECommerce.Customer.WebApi/Controllers/CustomerController.cs
+++ MicroserviceECommerce/MicroserviceECommerce.Customer.WebApi/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using MicroserviceECommerce.Entities;
+﻿using MicroserviceECommerce.Customer.WebApi.Models;
+using MicroserviceECommerce.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,32 @@ namespace MicroserviceECommerce.Customer.WebApi.Controllers
     public class CustomerController:ApiController
     {
         DataContext dataContext = new DataContext();
-        public List<Customers> GetCustomers()
+        public List<ModelCustomer> GetCustomers()
         {
-            List<Customers> customers= dataContext.Customers.ToList();
+            List<Customers> DataCustomersList= dataContext.Customers.ToList();
+            List<ModelCustomer> modelCustomersList = new List<ModelCustomer>();
+            foreach (var item in DataCustomersList)// item yakalanan Customer'e verdiğimiz değişken 
+            {
+                ModelCustomer modelCustomer = new ModelCustomer
+                {
+                    CustomerID = item.CustomerID,
+                    Address = item.Address,
+                    City = item.City,
+                    CompanyName = item.CompanyName,
+                    ContactName = item.ContactName,
+                    ContactTitle = item.ContactTitle,
+                    Country = item.Country,
+                    Fax = item.Fax,
+                    Password = item.Password,
+                    Phone = item.Phone,
+                    PostalCode = item.PostalCode
 
-            return customers;
+                };
+                modelCustomersList.Add(modelCustomer);
+              
+            }
+            return modelCustomersList;
         }
+        
     }
 }
