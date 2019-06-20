8f047ea61ba9a2fa3f94eab90fd59d24256bb1b7
fatih kaya <fatihkayabilgeadam@gmail.com>
2019-06-21 01:27:28 +0300

    Employee Listeleme yapıldı

---
+++ MicroserviceECommerce/MicroserviceECommerce.CORE.UI/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroserviceECommerce.Entities;
+using MicroserviceECommerce.MVCUI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceECommerce.CORE.UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employees> employeeList = HTTPHelper.SendRequest <List<Employees>>("http://localhost:37786/", "api/Employee/GetEmployees", RestSharp.Method.GET);
+            return View(employeeList);
+        }
+    }
+}
--- MicroserviceECommerce/MicroserviceECommerce.MVCUI/Controllers/CustomerController.cs
+++ MicroserviceECommerce/MicroserviceECommerce.MVCUI/Controllers/CustomerController.cs
@@ -12,9 +12,10 @@ namespace MicroserviceECommerce.MVCUI.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            
-           List<Customers> customerList = HTTPHelper.SendRequest<List<Customers>>("http://localhost:37776", "api/Customer/GetCustomers", RestSharp.Method.GET);
-             return View(customerList);
+
+            List<Customers> customerList = new List<Customers>();
+            customerList = HTTPHelper.SendRequest<List<Customers>>("http://localhost:37776", "api/Customer/GetCustomers", RestSharp.Method.GET);
+            return View(customerList);
         }
     }
 }
