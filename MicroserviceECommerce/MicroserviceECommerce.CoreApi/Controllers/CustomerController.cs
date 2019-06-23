3a8d7724b99b7e5db6c925b1d6198c449df33e0e
fatih kaya <fatihkayabilgeadam@gmail.com>
2019-06-23 17:24:57 +0300

    CoreApi Çalışmalarına başlandı

---
+++ MicroserviceECommerce/MicroserviceECommerce.CoreApi/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using MicroserviceECommerce.CoreApi.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroserviceECommerce.CoreApi.Controllers
+{
+    public class CustomerController:ControllerBase
+    {
+        NorthwindContext _northwindContext;
+        public CustomerController (NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+        [HttpGet]
+        public List<Customer> List()
+        {
+            var result = _northwindContext.Customers.ToList();
+            return result;
+        }
+    }
+}
---
+++ MicroserviceECommerce/MicroserviceECommerce.CoreApi/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroserviceECommerce.CoreApi.DataBase;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace MicroserviceECommerce.CoreApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "TestAPI", Version = "V1" });
+            });
+
+            var connection = @"Server=.\sqlexpress;Database=Northwind; user id=sa; password=123";
+            services.AddDbContext<NorthwindContext>
+                (options => options.UseSqlServer(connection));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "post API V1");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
